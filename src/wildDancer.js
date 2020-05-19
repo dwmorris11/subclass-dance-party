23330ac36e63b97dae8d75c4e74ab11a78f2428c
Dustin Morris <dustinnuc@gmail.com>
2020-05-19 01:38:13 -0500

    Updated WildDancer to function Updated dancefloor to work with wildDancer Updated styles.css to put in a dance floor

--- src/wildDancer.js
+++ src/wildDancer.js
@@ -14,12 +14,12 @@ MakeWildDancer.prototype.setPosition = function (top, left) {
   //if new top is less than zero then set new top to zero
   //if new left exceeds body.width then set new left to body.width
   //if new left is less than zero then set new left to zero
-  var min = -1;
-  var max = 1;
+  var min = -100;
+  var max = 100;
   var random1 = Math.floor(Math.random()*(max-min)) + min;
   var random2 = Math.floor(Math.random()*(max-min)) + min;
-  top = top + random1;
-  left = left + random2;
+  top = this.top + random1;
+  left = this.left + random2;
   if(top > $("body").height()) {
     top = $("body").height();
   }
@@ -33,5 +33,12 @@ MakeWildDancer.prototype.setPosition = function (top, left) {
     left = 0;
   }
   MakeDancer.prototype.setPosition.call(this, top, left);
+  var styleSettings = {
+    top: top,
+    left: left
+  };
+  this.$node.css(styleSettings);
+  MakeDancer.prototype.step.call(this);
+  setTimeout(this.setPosition.bind(this), this.timeBetweenSteps);
   this.$node.toggle();
 };
