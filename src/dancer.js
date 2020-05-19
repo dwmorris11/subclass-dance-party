8d6d203e144efedda8b781661ac682801b560448
Dustin Morris <dustinnuc@gmail.com>
2020-05-18 20:09:06 -0500

    Fixed modification of pseudoclassical by adding new keyword

--- src/dancer.js
+++ src/dancer.js
@@ -9,12 +9,14 @@ var MakeDancer = function(top, left, timeBetweenSteps) {
 };
 
 MakeDancer.prototype.step = function() {
+
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
-  setTimeout(this.step, timeBetweenSteps);
+
+  setTimeout(this.step, this.timeBetweenSteps);
 };
 
-MakerDancer.prototype.setPosition = function(top, left) {
+MakeDancer.prototype.setPosition = function(top, left) {
   // Use css top and left properties to position our <span> tag
   // where it belongs on the page. See http://api.jquery.com/css/
   //
